ee6fe7e3d44d2619c9e8daa851d834d3305f1592
Parithi <mail@parithi.com>
2015-12-28 12:08:05 +0530

    Final Commit for Project 1
    Final Commit for Project 1

--- app/src/main/java/com/parithi/movieexplorer/Constants.java
+++ app/src/main/java/com/parithi/movieexplorer/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 
     // API Params
     public static final String MOVIEDB_BASE_URL = "http://api.themoviedb.org/3/discover/movie?";
-    public final static String API_KEY = "3e9001bdbcd8d0e00e416d1826befaa5";
+    public static final String API_KEY = "[[ INSERT API KEY HERE ]]";
     public static final String SORT_PARAM = "sort_by";
     public static final String API_KEY_PARAM = "api_key";
     public static final String MOST_POPULAR_PARAM = "popularity.desc";
